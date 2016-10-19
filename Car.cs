9977d5e02ec02272165bc3314b0356db63589694
Bartłomiej Płoszyński <b.ploszynski@gmail.com>
2016-10-19 16:56:01 +0200

    dodalem ja
    opis ja

---
+++ Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class Car
+    {
+        public double pojemnoscSilnika;
+        public string marka;
+
+        public Car()
+        {
+        }
+        public Car(double pojemnoscSilnika, string marka)
+        {
+            this.pojemnoscSilnika = pojemnoscSilnika;
+            this.marka = marka;
+        }
+
+        public CarCreate()
+            this.
+    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car obiekt = new Car(2.1,"Ford");
+
+            Console.WriteLine("{0} {1}", obiekt.pojemnoscSilnika, obiekt.marka);
+            Console.ReadKey();
+        }
+    }
+}
